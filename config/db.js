a1e0a918c5cdc5bf9649603d0f0c473905a6b5ae
utkalika9 Nayak <utkalika.na@gmail.com>
2024-12-20 07:26:58 +0000

    prod-deploy:connected moongose

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/mydatabase');
+        await mongoose.connect('mongodb+srv://restaurantMgmt_admin:restaurantMgmt_admin@cluster0.4nv5l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Connected to MongoDB');
     } catch (err) {
         console.error('Failed to connect to MongoDB', err);
