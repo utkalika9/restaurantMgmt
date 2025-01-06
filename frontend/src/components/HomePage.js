1b43d889777e5d599d5d2708680dbe6bead2d3f1
utkalika9 Nayak <utkalika.na@gmail.com>
2025-01-06 03:17:10 +0000

    qrcode

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -173,7 +173,7 @@ const HomePage = () => {
           <Grid item xs={12} sm={6} md={4}>
             <Button
               component={Link}
-              to="/qrcode"
+              to="/qr-code"
               variant="contained"
               size="large"
               startIcon={<QrCodeIcon />}
